feda4e919f7900b9d406adeb2a172f71accc0922
sapatevaibhav <sapatevaibhav@duck.com>
2024-07-28 22:33:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import NewsComponent from "./components/NewsComponent";
+
+export default class App extends Component {
+  // name = "Vaibhav";
+  render() {
+    // let a = "Sapate";
+    return (
+      <div>
+        {/* Hello there {this.name + " " + a} */}
+        <Navbar />
+        <NewsComponent />
+      </div>
+    );
+  }
+}
---
+++ src/components/NewsComponent.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class NewsComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      page: 1,
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }&page=1&pageSize=21`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 21)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }&page=${this.state.page + 1}&pageSize=21`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }&page=${this.state.page - 1}&pageSize=21`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ page: this.state.page - 1, articles: parsedData.articles });
+  };
+
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>Top Headlines on NewsDonkey</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title.slice(0, 100) + "..."}
+                  // description={element.description}
+                  imageUrl={
+                    element.urlToImage === null
+                      ? "/android-chrome-512x512.png"
+                      : element.urlToImage
+                  }
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-outline-secondary"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-secondary"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsComponent;
